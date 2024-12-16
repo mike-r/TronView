17a9ddbe086a4d9b90c5db6a2a6265b2b9d153e5
Mike Rehberg <gitkraken@tr182.com>
2024-12-15 18:13:49 -0700

    Added testing to EGT/CHT decodes in serial_skyview.py

--- lib/inputs/serial_skyview.py
+++ lib/inputs/serial_skyview.py
@@ -185,7 +185,7 @@ class serial_skyview(Input):
                     msg = self.ser.read(222)
                     if isinstance(msg,str):msg = msg.encode() # if read from file then convert to bytes
                     HH,MM,SS,FF,OilPress,OilTemp, RPM_L,RPM_R,MAP,FF1,FF2,FP,FL_L,FL_R,Frem,V1,V2,AMPs,Hobbs,Tach,TC1,TC2,TC3,TC4,TC5,TC6,TC7,TC8,TC9,TC10,TC11,TC12,TC13,TC14,GP1,GP2,GP3,GP4,GP5,GP6,GP7,GP8,GP9,GP10,GP11,GP12,GP13,Contacts,Pwr,EGTstate,Checksum,CRLF= struct.unpack(
-                        "2s2s2s2s3s4s4s4s3s3s3s3s3s3s3s3s3s4s5s5s4s4s4s4s4s4s4s4s4s4s4s4s4s4s6s6s6s6s6s6s6s6s6s6s6s6s6s16s3s1s2s2s", msg
+                         "2s2s2s2s3s4s4s4s3s3s3s3s3s3s3s3s3s4s5s5s4s4s4s4s4s4s4s4s4s4s4s4s4s4s6s6s6s6s6s6s6s6s6s6s6s6s6s16s3s1s2s2s", msg
                     )
                     #print("EMS Message !3:", msg)
                     aircraft.sys_time_string = "%d:%d:%d"%(int(HH),int(MM),int(SS))
@@ -194,65 +194,29 @@ class serial_skyview(Input):
                     self.time_stamp_sec = int(SS)
 
                     aircraft.engine.OilPress = Input.cleanInt(self,OilPress)
-                    #print("Oil Pressure:"+str(OilPress)+"  :",aircraft.engine.OilPress, " PSI")
-
                     aircraft.engine.OilTemp = Input.cleanInt(self,OilTemp)
-                    #print("Oil Temp:"+str(OilTemp))
-
                     aircraft.engine.RPM = Input.cleanInt(self,RPM_L)
-                    #print("RPM:"+str(RPM_L)+"  :", aircraft.engine.RPM, " RPM")
-
                     aircraft.engine.ManPress = Input.cleanInt(self,MAP) * 0.1
-                    #print("MAP:"+str(MAP)+"   :", aircraft.engine.ManPress, " inHG")
-
                     aircraft.engine.FuelFlow = Input.cleanInt(self,FF1) / 10
-                    #print("Fuel Flow:"+str(FF1)+"  :", aircraft.engine.FuelFlow, " GPH")
-
                     aircraft.engine.FuelPress = Input.cleanInt(self,FP) / 10
-                    #print("Fuel Pressure:"+str(FP))
-
                     fuel_level_left  = Input.cleanInt(self, FL_L) / 10
                     fuel_level_right = Input.cleanInt(self, FL_R) / 10
                     aircraft.engine.FuelLevels = [fuel_level_left, fuel_level_right, 0, 0]
-                    #print("Fuel Level Left: "+str(FL_L)+"  :", aircraft.engine.FuelLevels[0], " Gallons Left Tank")
-                    #print("Fuel Level Right:"+str(FL_R)+"  :", aircraft.engine.FuelLevels[1], " Gallons Right Tank")
+                    aircraft.fuel.FuelLevels = [fuel_level_left, fuel_level_right, 0, 0]
 
-                    egt_1 = Input.cleanInt(self, TC12)
-                    aircraft.engine.EGT[0] = round((egt_1 * 1.8) + 32)  # convert from C to F
-                    cht_1 = Input.cleanInt(self, TC11)
-                    aircraft.engine.CHT[1] = round((cht_1 * 1.8) + 32)  # convert from C to F
+                    if TC12 != b'XXXX': aircraft.engine.EGT[0] = round(((Input.cleanInt(self, TC12)) * 1.8) + 32)  # convert from C to F
+                    if TC10 != b'XXXX': aircraft.engine.EGT[1] = round(((Input.cleanInt(self, TC10)) * 1.8) + 32)  # convert from C to F
+                    if TC8 != b'XXXX': aircraft.engine.EGT[2] = round(((Input.cleanInt(self, TC8)) * 1.8) + 32)  # convert from C to F
+                    if TC6 != b'XXXX': aircraft.engine.EGT[3] = round(((Input.cleanInt(self, TC6)) * 1.8) + 32)  # convert from C to F
+                    if TC4 != b'XXXX': aircraft.engine.EGT[4] = round(((Input.cleanInt(self, TC4)) * 1.8) + 32)  # convert from C to F
+                    if TC2 != b'XXXX': aircraft.engine.EGT[5] = round(((Input.cleanInt(self, TC2)) * 1.8) + 32)  # convert from C to F
 
-                    egt_2 = Input.cleanInt(self, TC10)
-                    aircraft.engine.EGT[1] = round((egt_2 * 1.8) + 32)  # convert from C to F
-                    cht_2 = Input.cleanInt(self, TC9)
-                    aircraft.engine.CHT[1] = round((cht_2 * 1.8) + 32)  # convert from C to F
-
-                    egt_3 = Input.cleanInt(self, TC8)
-                    aircraft.engine.EGT[2] = round((egt_3 * 1.8) + 32)  # convert from C to F
-
-                    egt_4 = Input.cleanInt(self, TC6)
-                    aircraft.engine.EGT[3] = round((egt_4 * 1.8) + 32)  # convert from C to F
-
-                    #egt_5 = Input.cleanInt(self, TC4)
-                    #aircraft.engine.EGT[4] = round((egt_5 * 1.8) + 32)  # convert from C to F
-
-                    #egt_6 = Input.cleanInt(self, TC2)
-                    #aircraft.engine.EGT[5] = round((egt_6 * 1.8) + 32)  # convert from C to F
-
-                    cht_1 = Input.cleanInt(self, TC11)
-                    aircraft.engine.CHT[0] = round((cht_1 * 1.8) + 32)  # convert from C to F
-
-                    cht_2 = Input.cleanInt(self, TC9)
-                    aircraft.engine.CHT[1] = round((cht_2 * 1.8) + 32)  # convert from C to F
-
-                    cht_3 = Input.cleanInt(self, TC7)
-                    aircraft.engine.CHT[2] = round((cht_3 * 1.8) + 32)  # convert from C to F
-
-                    cht_4 = Input.cleanInt(self, TC5)
-                    aircraft.engine.CHT[3] = round((cht_4 * 1.8) + 32)  # convert from C to F
-
-                    #cht_5 = Input.cleanInt(self, TC3)
-                    #cht_6 = Input.cleanInt(self, TC1)
+                    if TC11 != b'XXXX': aircraft.engine.CHT[0] = round(((Input.cleanInt(self, TC11)) * 1.8) + 32)  # convert from C to F
+                    if TC9 != b'XXXX': aircraft.engine.CHT[1] = round(((Input.cleanInt(self, TC9)) * 1.8) + 32)  # convert from C to F
+                    if TC7 != b'XXXX': aircraft.engine.CHT[2] = round(((Input.cleanInt(self, TC7)) * 1.8) + 32)  # convert from C to F
+                    if TC5 != b'XXXX': aircraft.engine.CHT[3] = round(((Input.cleanInt(self, TC5)) * 1.8) + 32)  # convert from C to F
+                    if TC3 != b'XXXX': aircraft.engine.EGT[4] = round(((Input.cleanInt(self, TC3)) * 1.8) + 32)  # convert from C to F
+                    if TC1 != b'XXXX': aircraft.engine.EGT[5] = round(((Input.cleanInt(self, TC1)) * 1.8) + 32)  # convert from C to F
 
                     if self.output_logFile != None:
                         Input.addToLog(self,self.output_logFile,bytes([33,int(dataType),int(dataVer)]))
